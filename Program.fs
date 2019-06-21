fb08f7146307701c42fe18e8285619b82416828e
Diego Costa <diego.costa@meta.com.br>
2019-06-21 02:25:04 -0300

    configured giraffe and dependency injection with static page

--- Program.fs
+++ Program.fs
@@ -1,8 +1,32 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open Giraffe
+open System.IO
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
 
-open System
+let webApp =
+    choose [ route "/about"   >=> text "Tutorial F# and SignalR : diegobassay@gmail.com" ]
+
+type Startup() =
+    member __.ConfigureServices (services : IServiceCollection) =
+        // Registrando serviços com as depedências do Giraffe
+        services.AddGiraffe() |> ignore
+        
+    member __.Configure (app : IApplicationBuilder) =
+        app.UseDefaultFiles() |> ignore
+        app.UseStaticFiles() |> ignore
+        app.UseGiraffe(webApp)
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot     = Path.Combine(contentRoot, "web")
+    WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(contentRoot)
+        .UseIISIntegration()
+        .UseWebRoot(webRoot)
+        .UseStartup<Startup>()
+        .Build()
+        .Run()
+    0
